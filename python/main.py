be24c02c08fce9ed28875e7733a8bd41812f0f81
Stephen BROIX <sbroix@gmail.com>
2019-07-14 19:10:56 +0200

    first version of code

---
+++ python/main.py
@@ -0,0 +1,63 @@
+#   Script Lancement Arrosage Jardin
+#   3 variables nécessaires :
+#       - Choix de la zone (1 : Herbe, 2 : potager+bambou)
+#       - durée en secondes
+#   exemple : lancement de la partie herbe pendant 5min --> python3 main.py 1 300
+
+import RPi.GPIO as GPIO
+import time
+import sys
+import tqdm
+
+def checkVariables():
+  if len(sys.argv) < 3:
+    print("variable(s) d'entrée(s) manquante(s)")
+    sys.exit()
+
+def launch(electrovannes,action):
+  for electrovanne in electrovannes:
+    GPIO.setup(electrovanne, GPIO.OUT,initial=GPIO.LOW)
+    if action == "launch":
+      print("Lancement de l'electrovanne " + str(electrovanne) + "...")
+      GPIO.output(electrovanne, GPIO.HIGH)
+      print("Electrovanne " + str(electrovanne) + " lancée")
+      
+    else:
+      print("Arret de l'electrovanne " + str(electrovanne) + "...")
+      GPIO.output(electrovanne, GPIO.LOW)
+      print("Electrovanne " + str(electrovanne) + " coupée")
+
+
+def main():
+  checkVariables()
+  duree=int(sys.argv[2])
+  if sys.argv[1]=="1":
+    type="herbe"
+    electrovannes=[36, 37]
+  else:
+    type="potager"
+    electrovannes=[38]
+
+  print("""
+------------------------------------------------
+         Activation de l'arrosage
+
+       - Lancement de l'arrosage partie : """ + type + """
+       - Durée : """ + str(duree) + """
+------------------------------------------------
+""")
+
+  print("Activation en mode 'BOARD'")
+  GPIO.setmode(GPIO.BOARD)
+
+  launch(electrovannes,"launch")
+
+  for i in tqdm.tqdm(range(duree)):
+    time.sleep(1)
+
+  launch(electrovannes,"stop")
+
+  print("Remise a zero des entrees/sorties")
+  GPIO.cleanup()
+
+main()
