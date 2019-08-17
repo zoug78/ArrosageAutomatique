407e1d2f38d233208a36e414e8512496fb4c95db
Stephen BROIX <sbroix@gmail.com>
2019-08-17 21:05:14 +0200

    Added firt version of API

---
+++ python/server.py
@@ -0,0 +1,21 @@
+import core
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/arrosage/herbe', methods=['POST'])
+def herbe():
+    if request.json["action"] == "on":
+        return core.main("on","herbe",request.json['temps'])
+    elif request.json["action"] == "off":
+        return core.stop_gpio()
+    
+@app.route('/api/v1.0/arrosage/potager', methods=['POST'])
+def potager():
+    if request.json["action"] == "on":
+        return core.main("on","potager",request.json['temps'])
+    elif request.json["action"] == "off":
+        return core.stop_gpio()
+
+if __name__ == '__main__':
+    app.run(debug=True)
